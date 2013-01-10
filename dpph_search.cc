e00bff5b40c7b232f0ac2a0537fd7d5b914b5653
grybka <grybka@ignatius.phys.washington.edu>
2013-01-10 12:29:23 -0800

    routine commit

--- dpph_search.cc
+++ dpph_search.cc
@@ -14,7 +14,7 @@ int handle_options(int argc,char *argv[]); //command line options
 //----changable parameters----
 //int fft_size=1024;
 int fft_size=256;
-double power_cut_dbm=-16;
+double power_cut_dbm=-25;
 string input_eggname;
 double freq_offset=0;
 char format='j';
